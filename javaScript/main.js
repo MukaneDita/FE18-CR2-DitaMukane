017b1ba81415803227204a8508c3991249abbc4d
Dita Mukane <dita.mukane@gmail.com>
2023-02-11 15:13:23 +0100

    third push

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,39 +1,43 @@
-// weekly tasks
-
-console.log(weeklyTasks);
 let wTasks = JSON.parse(weeklyTasks);
+console.log(wTasks);
 
 // cards from bootstrap
 for (let val of wTasks) {
     document.getElementById("tasks").innerHTML +=
-        `<div>
-        <div class="card p-2" style="width: 25rem;">
+        `<div  style=" padding-top: 2rem; padding-bottom: 2rem">
+        <div class="card p-2" style="width: 23rem; height: 47rem">
         <li class="list-group-item"><span class="bg-primary text-white mb-2 py-0.75 px-1 rounded-2">Task</span></li>
-        <img src="${val.image}" class="card-img-top mt-1" alt="${val.taskName}">
-        <div class="card-body">
+        <img src="${val.image}" class="card-img-top mt-1"  alt="${val.taskName}"> 
+        <div class="card-body text-center" >
           <h5 class="card-title">${val.taskName}</h5>
-          <p class="card-text">${val.description}</p>
+          <p class="card-text ">${val.description}</p>
         </div>
         <ul class="list-group list-group-flush">
-          <li class="list-group-item "><i class="fa-solid fa-triangle-exclamation "style="width: 1rem;"></i> <span class="fw-bold">Priority level:<p class="btn btn-success priorityBtn h-auto" style="width:1rem;">${val.importance}</p> </span><br><i class="fa-solid fa-map-location-dot"style="width:1rem;" ></i> <span class="fw-bold">Location:</span> ${val.location}</li>
-          <li class="list-group-item"></li>
+          <li class="list-group-item ">
+            <i class="fa-solid fa-triangle-exclamation "style="width: 1rem"></i> 
+              <span class="fw-bold">Priority level:<p class="btn btn-success id="clickOnThis" h-auto" likeBtn>${val.importance}</p></span><br>
+              <i class="fa-solid fa-map-location-dot"style="width:1rem;" ></i> 
+              <span class="fw-bold">Location:</span> ${val.location}</li>
+          <li class="list-group-item float-end"> 
+            <p class="btn btn-danger id="buttons" h-auto" likeBtn>Delete</p>
+            <p class="btn btn-success id="buttons" h-auto" likeBtn>Done</p>
+          </li>
         </ul>
       </div>
       </div>`;
 }
-
-let btns = document.getElementsByName("priorityBtn");
-let importance = document.getElementById("importance").value;
+let btns = document.getElementsByName("likeBtn");
 for (let i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function() {
-
+        wTasks[i].importance++;
         console.log(wTasks[i].importance);
-        document.getElementById("importance")[i].innerHTML = wTasks[i].importance;
+        document.getElementById("clickOnThis")[i].innerHTML = wTasks[i].importance;
     })
-
-    if (importance.length >= 5) {
-        document.getElementById("color").style.color = "green";
+    if (btns.length < 2) {
+        document.getElementById("clickOnThis").style.backgroundColor = "green";
+    } else if (btns.length < 4) {
+        document.getElementById("clickOnThis").style.backgroundColor = "yellow";
     } else {
-        document.getElementById("color").style.color = "red";
+        document.getElementById("clickOnThis").style.backgroundColor = "red";
     }
 }
